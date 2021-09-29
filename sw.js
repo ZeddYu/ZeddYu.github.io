f76d2820373a7ad45e8d65eb7004caf080f30eef
ZeddYu <ZeddYu@users.noreply.github.com>
2021-09-29 16:54:15 +0000

    deploy: ZeddYu/Blog-Hugo@c5e4069e1e4766d2e26a2591917aa34813997cb7

---
+++ sw.js
@@ -0,0 +1,50 @@
+importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js");
+
+if (workbox) {
+	console.log(`Yay! Workbox is loaded 🎉`);
+
+	workbox.routing.registerRoute(
+		/\.(?:js|css)$/,
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: "static-resources",
+		})
+	);
+
+	workbox.routing.registerRoute(
+		/\.(?:png|jpg|jpeg|svg|gif)$/,
+		new workbox.strategies.CacheFirst({
+			cacheName: "image-cache",
+			plugins: [
+				new workbox.expiration.ExpirationPlugin({
+					maxEntries: 20,
+					maxAgeSeconds: 7 * 24 * 60 * 60,
+				}),
+			],
+		})
+	);
+
+	workbox.routing.registerRoute(
+		/^https:\/\/fonts\.googleapis\.com/,
+		new workbox.strategies.StaleWhileRevalidate({
+			cacheName: "google-fonts-stylesheets",
+		})
+	);
+
+	workbox.routing.registerRoute(
+		/^https:\/\/fonts\.gstatic\.com/,
+		new workbox.strategies.CacheFirst({
+			cacheName: "google-fonts-webfonts",
+			plugins: [
+				new workbox.cacheableResponse.CacheableResponsePlugin({
+					statuses: [0, 200],
+				}),
+				new workbox.expiration.ExpirationPlugin({
+					maxAgeSeconds: 60 * 60 * 24 * 365,
+					maxEntries: 30,
+				}),
+			],
+		})
+	);
+} else {
+	console.log(`Boo! Workbox didn't load 😬`);
+}
